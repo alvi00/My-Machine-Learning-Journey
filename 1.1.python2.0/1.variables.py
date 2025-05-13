b0a0d5962bdf7294ef77254255197830c32a4349
alvi00 <ahmadfahmid59@gmail.com>
2025-05-13 20:39:55 +0600

    added some files

---
+++ 1.1.python2.0/1.variables.py
@@ -0,0 +1,15 @@
+#find out the total montly expenses given individual expenses items and also find yearly expense
+
+current_bill=10
+
+water_bill=10.6
+
+total_bill=current_bill+water_bill
+
+print("So the total bill is ",total_bill)
+
+year_bill=total_bill*12
+
+print("So the total yearly bill is ", round(year_bill,2))
+
+#round(number_to_be_rounded,number will apear after point)
