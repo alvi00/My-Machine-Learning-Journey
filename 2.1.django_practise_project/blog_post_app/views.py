2b27aa063802b8789797178df6088a5177a2af55
alvi00 <ahmadfahmid59@gmail.com>
2025-06-12 20:00:24 +0600

    added new pratice project

--- 2.1.django_practise_project/blog_post_app/views.py
+++ 2.1.django_practise_project/blog_post_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from . models import post
 # Create your views here.
+
+def home(request):
+    posts=post.objects.all()
+    return render(request,"home_page/home.html",{"posts":posts})
