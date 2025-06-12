4029cc927e9fd1f9dbc0400dd45ee06459a76de7
alvi00 <ahmadfahmid59@gmail.com>
2025-06-12 17:44:14 +0600

    minor fix

--- 2.django/blog_app/models.py
+++ 2.django/blog_app/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Post(models.Model):
     tittle=models.CharField(max_length=200)
-    content=models.TextField
+    content=models.TextField(null=True, blank=True)
     created_at=models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- 2.django/readme.md
+++ 2.django/readme.md
@@ -17,3 +17,5 @@ to active the venv
 8. then to migrate i had to write python manage.py migrate
  
 9. to create a dashboard i had to write python manage.py createsuperuser
+
+10. to run the server i has to write python manage.py runserver
